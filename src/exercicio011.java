4a9e4babfa8788c2eacab69dd5d4800fe24bb0ee
gustavocardoso2000 <gustavoccardoso2000@gmail.com>
2022-11-28 20:17:53 -0300

    first commit

---
+++ src/exercicio011.java
@@ -0,0 +1,20 @@
+
+public class exercicio011 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+
+        System.out.println("|\\__________/|");
+        System.out.println("|  O  |   O  |");
+        System.out.println("| =  ____ =  |");
+        System.out.println("|____________|");
+
+
+        
+        
+        
+        
+        
+        
+    } 
+}
